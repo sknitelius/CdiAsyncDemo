e9d2920d9c16084fc76ca114b8208d31431e7764
Stephan Knitelius <stephan@knitelius.com>
2016-03-06 12:15:21 +0100

    clean-up

--- src/main/java/science/raketen/cdiasyncdemo/HelloBoundary.java
+++ src/main/java/science/raketen/cdiasyncdemo/HelloBoundary.java
@@ -19,7 +19,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 import javax.enterprise.context.RequestScoped;
-import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -46,8 +45,4 @@ public class HelloBoundary {
     return "Response by: " + Thread.currentThread().getName() + " Msg: " + helloFuture.get();
   }
 
-  public void listen(@Observes String sysout) {
-    int i = 1 + 1;
-    String x = (i / 2) + sysout;
-  }
 }
